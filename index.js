9740e7a86f3a817421a17e4d78a053264b5cc468
Cunning-Fox-99 <kostya.moroz.96@gmail.com>
2024-02-08 09:55:26 +0200

    init

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const port = 8000;
 
 app.use(express.json());
 
+// Добавляем middleware для обработки CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Разрешаем все домены. Для продакшн приложений лучше указать конкретный домен.
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200); // Отправляем 200 для предварительных запросов
+    }
+    next();
+});
+
+
 app.post('/get-sha1', (req, res) => {
     const { data } = req.body;
     if (!data) {
